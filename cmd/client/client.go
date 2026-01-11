51d8ac63d29353cbbd378bde068763deaa7b5ffe
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-11 17:12:01 -0500

    feat: added the client code

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,1 +1,32 @@
 package client
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+//client handles 2 things concurrently
+// reading from the network
+// and reading from the keyboard
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan struct{})
+
+	//goroutine to handle the background server output
+	go func() {
+		io.Copy(os.Stdout, conn)
+		log.Println("done")
+		done <- struct{}{}
+	}()
+
+	io.Copy(conn, os.Stdin)
+	conn.Close()
+	<-done // waiting for the background goroutine to finish
+}
